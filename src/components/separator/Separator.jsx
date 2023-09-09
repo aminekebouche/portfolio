44d9c5044302a05643b27a18e0e4c1b6a0d29022
aminekebouche <keboucheamine6@gmail.com>
2023-09-09 16:41:55 +0200

    responsive remove bg

--- src/components/separator/Separator.jsx
+++ src/components/separator/Separator.jsx
@@ -1,35 +1,11 @@
 import React from "react";
+import "./separator.scss";
 
 const Separator = () => {
   return (
-    <div
-      className="separator"
-      style={{
-        backgroundImage: "url(/images/sep.jpg)",
-        width: "100%",
-        height: "350px",
-        backgroundSize: "cover",
-        marginBottom: "200px",
-        backgroundAttachment: "fixed",
-        display: "flex",
-        justifyContent: "center",
-        position: "relative",
-      }}
-    >
-      <h1 style={{ color: "white", textAlign: "center" }}>
-        Développement Web Responsive
-      </h1>
-      <img
-        src="/images/resp.png"
-        alt=""
-        className="separator-responsive-img"
-        style={{
-          position: "absolute",
-          bottom: "-100px",
-          left: "50%",
-          transform: "translateX(-50%)",
-        }}
-      />
+    <div className="separator">
+      <h1>Développement Web Responsive</h1>
+      <img src="/images/resp.png" alt="" className="separator-responsive-img" />
     </div>
   );
 };
